91d1ec35ca8ce4bf3b55126f5a7404d491251e63
Diana <20200005@ricaldone.edu.sv>
2024-05-22 17:34:29 -0600

    Creación de página de carga

---
+++ app/src/main/java/diana/padilla/isss_salud/activity_pantalla_carga.kt
@@ -0,0 +1,30 @@
+package diana.padilla.isss_salud
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_pantalla_carga : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pantalla_carga)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val pantallaCarga = 3000
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, pantallaCarga.toLong())
+    }
+}
