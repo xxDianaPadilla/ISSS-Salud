ae90b271366527875aeff17cf7d68964de4780f1
Diana <20200005@ricaldone.edu.sv>
2024-05-23 20:35:43 -0600

    Creación de pantalla de bienvenida

--- app/src/main/java/diana/padilla/isss_salud/activity_ingreso.kt
+++ app/src/main/java/diana/padilla/isss_salud/activity_ingreso.kt
@@ -1,29 +1,20 @@
 package diana.padilla.isss_salud
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class activity_ingreso : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_ingreso)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnPantallaLogin = findViewById<Button>(R.id.btnPantallaLogin)
-
-        btnPantallaLogin.setOnClickListener {
-            val pantallaLogin = Intent(this, activity_ingreso::class.java)
-            startActivity(pantallaLogin)
         }
     }
 }
