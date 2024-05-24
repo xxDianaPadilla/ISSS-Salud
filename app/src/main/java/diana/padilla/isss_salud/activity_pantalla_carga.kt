a40f8849f644007bd18ecd1d4e1981695e12763d
Diana <20200005@ricaldone.edu.sv>
2024-05-23 18:42:58 -0600

    Cambio de logo, dependiendo de tema

--- app/src/main/java/diana/padilla/isss_salud/activity_pantalla_carga.kt
+++ app/src/main/java/diana/padilla/isss_salud/activity_pantalla_carga.kt
@@ -1,8 +1,10 @@
 package diana.padilla.isss_salud
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +19,15 @@ class activity_pantalla_carga : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val logoISSS = findViewById<ImageView>(R.id.IvLogoIsss)
+        val modoOscuro = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        if(modoOscuro == Configuration.UI_MODE_NIGHT_YES){
+            logoISSS.setImageResource(R.drawable.ic_modo_oscuro_logo)
+        }else{
+            logoISSS.setImageResource(R.drawable.id_logo_isss)
         }
 
         val pantallaCarga = 3000
