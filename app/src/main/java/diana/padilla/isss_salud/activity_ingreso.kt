13da7dd652301ebc640e219ad13532c3022b1aa0
Diana <20200005@ricaldone.edu.sv>
2024-05-24 15:43:18 -0600

    Pantalla modo oscuro en la pantalla de login

--- app/src/main/java/diana/padilla/isss_salud/activity_ingreso.kt
+++ app/src/main/java/diana/padilla/isss_salud/activity_ingreso.kt
@@ -1,6 +1,8 @@
 package diana.padilla.isss_salud
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,15 @@ class activity_ingreso : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val logoISSS = findViewById<ImageView>(R.id.IvLogoIsss)
+        val modoOscuro = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        if(modoOscuro == Configuration.UI_MODE_NIGHT_YES){
+            logoISSS.setImageResource(R.drawable.ic_modo_oscuro_logo)
+        }else{
+            logoISSS.setImageResource(R.drawable.id_logo_isss)
         }
     }
 }
