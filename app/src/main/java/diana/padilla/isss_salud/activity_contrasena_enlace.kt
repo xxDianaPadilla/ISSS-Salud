f6865323c4382ad63b74fb4d7a750ca0b1314a96
Diana <20200005@ricaldone.edu.sv>
2024-05-25 14:38:12 -0600

    Agregué modo oscuro a la pantalla de recuperación por enlace

--- app/src/main/java/diana/padilla/isss_salud/activity_contrasena_enlace.kt
+++ app/src/main/java/diana/padilla/isss_salud/activity_contrasena_enlace.kt
@@ -1,8 +1,11 @@
 package diana.padilla.isss_salud
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +18,42 @@ class activity_contrasena_enlace : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val logoISSS = findViewById<ImageView>(R.id.IvLogoIsss)
+        val modoOscuro = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        if(modoOscuro == Configuration.UI_MODE_NIGHT_YES){
+            logoISSS.setImageResource(R.drawable.ic_modo_oscuro_logo)
+        }else{
+            logoISSS.setImageResource(R.drawable.id_logo_isss)
+        }
+
+        val fondoCirculo = findViewById<ConstraintLayout>(R.id.clFondoCirculo)
+        val modoOscuro2 = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        if(modoOscuro2 == Configuration.UI_MODE_NIGHT_YES){
+            fondoCirculo.setBackgroundResource(R.drawable.ic_circulo_oscuro)
+        }else{
+            fondoCirculo.setBackgroundResource(R.drawable.ic_circulo)
+        }
+
+        val fondoIcon = findViewById<ConstraintLayout>(R.id.clLock)
+        val modoOscuro3 = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        if(modoOscuro3 == Configuration.UI_MODE_NIGHT_YES){
+            fondoIcon.setBackgroundResource(R.drawable.ic_dark_lock)
+        }else{
+            fondoIcon.setBackgroundResource(R.drawable.ic_lock)
+        }
+
+        val orLines = findViewById<ImageView>(R.id.ivOrLine)
+        val modoOscuro4 = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        if(modoOscuro4 == Configuration.UI_MODE_NIGHT_YES){
+            orLines.setImageResource(R.drawable.or_dark_line)
+        }else{
+            orLines.setImageResource(R.drawable.or_lines)
         }
     }
 }
