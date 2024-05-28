55f8e3aece05b4612a8b3d4526727a58cbed175f
Diana <20200005@ricaldone.edu.sv>
2024-05-27 18:51:31 -0600

    Agregué modo oscuro a la página de noticias

--- app/src/main/java/diana/padilla/isss_salud/activity_noticias.kt
+++ app/src/main/java/diana/padilla/isss_salud/activity_noticias.kt
@@ -1,6 +1,8 @@
 package diana.padilla.isss_salud
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,24 @@ class activity_noticias : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val logoIsssSmall = findViewById<ImageView>(R.id.ivSmallLogo)
+        val modoOscuro = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        if(modoOscuro == Configuration.UI_MODE_NIGHT_YES){
+            logoIsssSmall.setImageResource(R.drawable.ic_logo_isss_dark)
+        }else{
+            logoIsssSmall.setImageResource(R.drawable.ic_logo_isss_small)
+        }
+
+        val iconNotificaciones = findViewById<ImageView>(R.id.ivNotificaciones)
+        val modoOscuro2 = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        if(modoOscuro2 == Configuration.UI_MODE_NIGHT_YES){
+            iconNotificaciones.setImageResource(R.drawable.ic_darkmode_notificaciones)
+        }else{
+            iconNotificaciones.setImageResource(R.drawable.ic_notificaciones)
         }
     }
 }
