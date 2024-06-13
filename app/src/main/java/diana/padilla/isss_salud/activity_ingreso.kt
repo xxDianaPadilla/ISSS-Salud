9464b83347b3c802970425afdc173e4c302b6bb6
LuisEscalante <20200602@ricaldone.edu.sv>
2024-06-12 18:27:51 -0600

    Agregue codigo en el login

--- app/src/main/java/diana/padilla/isss_salud/activity_ingreso.kt
+++ app/src/main/java/diana/padilla/isss_salud/activity_ingreso.kt
@@ -1,15 +1,22 @@
 package diana.padilla.isss_salud
 
+import Modelo.ClaseConexion
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class activity_ingreso : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,16 +29,62 @@ class activity_ingreso : AppCompatActivity() {
             insets
         }
 
+        val btnRegistrarse = findViewById<Button>(R.id.btnRegistrarse)
+        val txtCorreo = findViewById<EditText>(R.id.txtCorreo)
+        val txtContrasena = findViewById<EditText>(R.id.txtContrasena)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
         val logoISSS = findViewById<ImageView>(R.id.IvLogoIsss)
         val modoOscuro = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        val correoPattern = "[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}".toRegex()
+
+
+        btnLogin.setOnClickListener {
+
+            val correo = txtCorreo.text.toString()
+            val contrasena = txtContrasena.text.toString()
+
+            if (correo.isEmpty() || contrasena.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Error, para acceder debes llenar todas las casillas.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else if (!correoPattern.matches(correo)) {
+                Toast.makeText(
+                    this,
+                    "El correo electrónico no es valido.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Log.i("Test de credenciales", "Correo: $correo y Contraseña: $contrasena")
+            }
+        }
+
+        btnLogin.setOnClickListener {
+            val pantallaNoticias = Intent(this, activity_noticias::class.java)
+            CoroutineScope(Dispatchers.IO).launch {
+
+                val objConexion = ClaseConexion().cadenaConexion()
+
+                val comprobarUsuario =
+                    objConexion?.prepareStatement("SELECT * FROM Usuarios WHERE correo_electronico = ? AND contrasena = ?")!!
+                    comprobarUsuario.setString(1, txtCorreo.text.toString())
+                    comprobarUsuario.setString(2, txtContrasena.text.toString())
+
+                    val resultado = comprobarUsuario.executeQuery()
+                    if (resultado.next()) {
+                        startActivity(pantallaNoticias)
+                    } else {
+                        println("Usuario no encontrado, verifique las credenciales")
+                    }
+            }
+        }
 
         if(modoOscuro == Configuration.UI_MODE_NIGHT_YES){
             logoISSS.setImageResource(R.drawable.ic_modo_oscuro_logo)
         }else{
             logoISSS.setImageResource(R.drawable.id_logo_isss)
         }
-
-        val btnRegistrarse = findViewById<Button>(R.id.btnRegistrarse)
 
         btnRegistrarse.setOnClickListener {
             val pantallaRegistrarse = Intent(this, activity_registrarse::class.java)
@@ -43,13 +96,6 @@ class activity_ingreso : AppCompatActivity() {
         txtOlvidoContrasena.setOnClickListener {
             val pantallaOlvidoContrasena = Intent(this, activity_contrasena_enlace::class.java)
             startActivity(pantallaOlvidoContrasena)
-        }
-
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
-
-        btnLogin.setOnClickListener {
-            val pantallaNoticias = Intent(this, activity_noticias::class.java)
-            startActivity(pantallaNoticias)
         }
     }
 }
