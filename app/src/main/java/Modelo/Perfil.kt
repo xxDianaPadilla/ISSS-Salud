f5d041265bdb303d9a3d394030523f6a4d95b18d
Diana <20200005@ricaldone.edu.sv>
2024-07-08 13:26:11 -0600

    Esto aún no funciona ...

---
+++ app/src/main/java/Modelo/Perfil.kt
@@ -0,0 +1,9 @@
+package Modelo
+
+data class Perfil(
+    val dui: String,
+    val tipo_sangre: String,
+    val telefono: String,
+    val foto_usuario: String,
+    val correo_electronico: String
+)
--- app/src/main/java/diana/padilla/isss_salud/activity_perfil.kt
+++ app/src/main/java/diana/padilla/isss_salud/activity_perfil.kt
@@ -1,15 +1,23 @@
 package diana.padilla.isss_salud
 
+import Modelo.ClaseConexion
+import Modelo.Perfil
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class activity_perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +28,56 @@ class activity_perfil : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val imagenPerfil = findViewById<ImageView>(R.id.ImgPerfilSinCargar)
+        val correoPerfilE = findViewById<EditText>(R.id.txtViewCorreoPerfilE)
+        val telefonoPerfilE = findViewById<EditText>(R.id.txtViewTelefonoPerfilE)
+        val duiPerfilE = findViewById<EditText>(R.id.txtViewDuiPerfilE)
+        val tipoSangreE = findViewById<EditText>(R.id.txtViewTipoSangreE)
+
+        fun obtenerDatosPerfil(): List<Perfil>{
+            val perfil = mutableListOf<Perfil>()
+
+            CoroutineScope(Dispatchers.IO).launch {
+                val objConexion = ClaseConexion().cadenaConexion()
+                val correoDeLaVariableGlobal = activity_ingreso.variablesGlobales.miMorreo
+                val obtenerPerfil =
+                    objConexion?.prepareStatement("SELECT foto_usuario, dui, correo_electronico, telefono, tipo_sangre FROM Usuarios WHERE correo_electronico = ?")!!
+                obtenerPerfil.setString(1, correoDeLaVariableGlobal)
+                val resultSet = obtenerPerfil.executeQuery()
+
+                while (resultSet.next()){
+                    var foto_usuario: String = resultSet.getString("foto_usuario")
+                    var correo_electronico: String = resultSet.getString("correo_electronico")
+                    var telefono: String = resultSet.getString("telefono")
+                    var dui: String = resultSet.getString("dui")
+                    var tipo_sangre: String = resultSet.getString("tipo_sangre")
+
+                    val perfilCompleto = Perfil(foto_usuario, correo_electronico, telefono, dui, tipo_sangre)
+                    perfil.add(perfilCompleto)
+                }
+            }
+
+            return perfil
+        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val perfiles = withContext(Dispatchers.IO) { obtenerDatosPerfil() }
+            if (perfiles.isNotEmpty()){
+                val miFoto = perfiles[0].foto_usuario
+                Glide.with(this@activity_perfil)
+                    .load(miFoto)
+                    .into(imagenPerfil)
+                val miDui = perfiles[0].dui
+                duiPerfilE.hint = miDui
+                val miCorreo = perfiles[0].correo_electronico
+                correoPerfilE.hint = miCorreo
+                val miTelefono = perfiles[0].telefono
+                telefonoPerfilE.hint = miTelefono
+                val miTipoSangre = perfiles[0].tipo_sangre
+                tipoSangreE.hint = miTipoSangre
+            }
         }
 
         val logoIsssSmall = findViewById<ImageView>(R.id.ivSmallLogo)
