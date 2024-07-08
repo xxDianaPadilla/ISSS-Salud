43a7d5d09d32055444f5ab03834e0622541ca0aa
Diana <20200005@ricaldone.edu.sv>
2024-07-08 14:58:17 -0600

    Ahora si funciona eln código

--- app/src/main/java/Modelo/Perfil.kt
+++ app/src/main/java/Modelo/Perfil.kt
@@ -1,9 +1,9 @@
 package Modelo
 
 data class Perfil(
-    val dui: String,
-    val tipo_sangre: String,
-    val telefono: String,
     val foto_usuario: String,
-    val correo_electronico: String
+    val correo_electronico: String,
+    val telefono: String,
+    val dui: String,
+    val tipo_sangre: String
 )
