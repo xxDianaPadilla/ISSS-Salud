3dead216b065d03c109a3035168e8408be9b3a4a
Diana <20200005@ricaldone.edu.sv>
2024-07-08 20:52:34 -0600

    Edición de perfil casi terminada

--- app/src/main/java/diana/padilla/isss_salud/activity_editar_perfil.kt
+++ app/src/main/java/diana/padilla/isss_salud/activity_editar_perfil.kt
@@ -1,13 +1,21 @@
 package diana.padilla.isss_salud
 
+import Modelo.ClaseConexion
+import Modelo.Perfil
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class activity_editar_perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +26,60 @@ class activity_editar_perfil : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val imagenPerfil = findViewById<ImageView>(R.id.ImgPerfilSinCargar2)
+        val txtCorrePerfil = findViewById<EditText>(R.id.txtCorrePerfil)
+        val txtViewTelefonoPerfil = findViewById<EditText>(R.id.txtViewTelefonoPerfil)
+        val txtViewDuiPerfil = findViewById<EditText>(R.id.txtViewDuiPerfil)
+        val txtViewTipoSangre = findViewById<EditText>(R.id.txtViewTipoSangre)
+
+        fun datosPerfil(): List<Perfil> {
+            val perfil2 = mutableListOf<Perfil>()
+
+            CoroutineScope(Dispatchers.IO).launch {
+                val objConexion = ClaseConexion().cadenaConexion()
+                val correoDeLaVariableGlobal = activity_ingreso.variablesGlobales.miMorreo
+
+                val obtenerPerfil2 =
+                    objConexion?.prepareStatement("SELECT foto_usuario, dui, correo_electronico, telefono, tipo_sangre FROM Usuarios WHERE correo_electronico = ?")!!
+                obtenerPerfil2.setString(1, correoDeLaVariableGlobal)
+                val resultSet = obtenerPerfil2.executeQuery()
+
+                while (resultSet.next()) {
+                    var foto_usuario2: String = resultSet.getString("foto_usuario")
+                    var correo_electronico2: String = resultSet.getString("correo_electronico")
+                    var telefono2: String = resultSet.getString("telefono")
+                    var dui2: String = resultSet.getString("dui")
+                    var tipo_sangre2: String = resultSet.getString("tipo_sangre")
+
+                    val perfilCompleto2 =
+                        Perfil(foto_usuario2, correo_electronico2, telefono2, dui2, tipo_sangre2)
+                    perfil2.add(perfilCompleto2)
+
+                    println(perfilCompleto2)
+                }
+            }
+
+            return perfil2
+        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val perfiles2 = withContext(Dispatchers.IO) { datosPerfil() }
+            if (perfiles2.isNotEmpty()){
+                val miFoto2 = perfiles2[0].foto_usuario
+                Glide.with(this@activity_editar_perfil)
+                    .load(miFoto2)
+                    .into(imagenPerfil)
+                val miDui2 = perfiles2[0].dui
+                txtViewDuiPerfil.hint = miDui2
+                val miCorreo2 = perfiles2[0].correo_electronico
+                txtCorrePerfil.hint = miCorreo2
+                val miTelefono2 = perfiles2[0].telefono
+                txtViewTelefonoPerfil.hint = miTelefono2
+                val miTipoSangre2 = perfiles2[0].tipo_sangre
+                txtViewTipoSangre.hint = miTipoSangre2
+            }
         }
 
         val logoIsssSmall = findViewById<ImageView>(R.id.ivSmallLogo)
