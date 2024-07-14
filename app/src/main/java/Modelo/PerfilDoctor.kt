d9343d238be9cb46b6a7a607fb4f6aa80ece91fc
Dixi <20200201@ricaldone.edu.sv>
2024-07-13 18:17:30 -0600

    Carga de datos de doctores casi terminado

---
+++ app/src/main/java/Modelo/PerfilDoctor.kt
@@ -0,0 +1,9 @@
+package Modelo
+
+data class PerfilDoctor(
+    val foto_doctor_url: String,
+    val correo_doctor: String,
+    val nombre_doctor: String,
+    val especialidad: String,
+    val unidad_medica: String
+)
--- app/src/main/java/diana/padilla/isss_salud/activity_perfil_doctor.kt
+++ app/src/main/java/diana/padilla/isss_salud/activity_perfil_doctor.kt
@@ -1,14 +1,24 @@
 package diana.padilla.isss_salud
 
+import Modelo.ChatsDoctores
+import Modelo.CitasAgendadas
+import Modelo.ClaseConexion
+import Modelo.PerfilDoctor
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class activity_perfil_doctor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +29,70 @@ class activity_perfil_doctor : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val ImgDoctorPerfil2 = findViewById<ImageView>(R.id.ImgDoctorPerfil2)
+        val txtViewCorreo = findViewById<TextView>(R.id.txtViewCorreo)
+        val txtViewNombreDoctorP = findViewById<TextView>(R.id.txtViewNombreDoctorP)
+        val txtViewEspecialidadDoctor = findViewById<TextView>(R.id.txtViewEspecialidadDoctor)
+        val txtViewUnidadMedicaDoctor = findViewById<TextView>(R.id.txtViewUnidadMedicaDoctor)
+
+        fun obtenerDoctor(): List<PerfilDoctor>{
+            val doctor = mutableListOf<PerfilDoctor>()
+
+            CoroutineScope(Dispatchers.IO).launch {
+                val objConexion = ClaseConexion().cadenaConexion()
+                val obtenerDoctor =
+                    objConexion?.prepareStatement("""
+        SELECT 
+            d.foto_doctor,
+            d.correo_doctor,
+            d.nombre_doctor,
+            ed.especialidad_doctor AS Especialidad_Medica,
+            um.nombre_unidad AS Unidad_Medica
+        FROM 
+            Doctores d
+        INNER JOIN 
+            EspecialidadDoctores ed ON d.id_especialidad = ed.id_especialidad
+        INNER JOIN 
+            UnidadesMedicas um ON d.id_unidad = um.id_unidad
+        WHERE 
+            d.foto_doctor = 'https://familydoctor.org/wp-content/uploads/2018/02/41808433_l.jpg'
+    """)!!
+                val resultSet = obtenerDoctor.executeQuery()
+
+                while (resultSet.next()){
+                    var foto_doctor: String = resultSet.getString("foto_doctor")
+                    var correo_doctor: String = resultSet.getString("correo_doctor")
+                    var nombre_doctor: String = resultSet.getString("nombre_doctor")
+                    var Especialidad_Medica: String = resultSet.getString("Especialidad_Medica")
+                    var Unidad_Medica: String = resultSet.getString("Unidad_Medica")
+
+                    val doctorCompleto = PerfilDoctor(foto_doctor, correo_doctor, nombre_doctor, Especialidad_Medica, Unidad_Medica)
+                    println(doctorCompleto)
+                    doctor.add(doctorCompleto)
+                }
+            }
+
+            return doctor
+        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val doctores = withContext(Dispatchers.IO) {obtenerDoctor()}
+            if (doctores.isNotEmpty()){
+                val fotoDoctor = doctores[0].foto_doctor_url
+                Glide.with(this@activity_perfil_doctor)
+                    .load(fotoDoctor)
+                    .into(ImgDoctorPerfil2)
+                val correoDoctor = doctores[0].correo_doctor
+                txtViewCorreo.text = correoDoctor
+                val nombreDoctor = doctores[0].nombre_doctor
+                txtViewNombreDoctorP.text = nombreDoctor
+                val EspecialidadMedica = doctores[0].especialidad
+                txtViewEspecialidadDoctor.text = EspecialidadMedica
+                val UnidadMedica = doctores[0].unidad_medica
+                txtViewUnidadMedicaDoctor.text = UnidadMedica
+            }
         }
 
         val btnRegresar = findViewById<ImageView>(R.id.btnRegresar)
