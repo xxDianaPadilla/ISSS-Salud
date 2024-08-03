1ca27d337d476593fda5cd1cbac465a35db3b410
LuisEscalante21 <20200602@ricaldone.edu.sv>
2024-08-03 14:33:46 -0600

    Mejoré las validaciones

--- app/src/main/java/diana/padilla/isss_salud/activity_registrarse.kt
+++ app/src/main/java/diana/padilla/isss_salud/activity_registrarse.kt
@@ -57,14 +57,16 @@ class activity_registrarse : AppCompatActivity() {
 
         val spTipoSangre = findViewById<Spinner>(R.id.spTipoSangre)
         val listaTipoSangre = listOf("A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-")
-        val adapterTipoSangre = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, listaTipoSangre)
+        val adapterTipoSangre =
+            ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, listaTipoSangre)
         spTipoSangre.adapter = adapterTipoSangre
 
 
 
-        fun hashSHA256(contrasenaEncriptada: String): String{
-            val bytes = MessageDigest.getInstance("SHA-256").digest(contrasenaEncriptada.toByteArray())
-            return bytes.joinToString(""){ "%02x".format(it) }
+        fun hashSHA256(contrasenaEncriptada: String): String {
+            val bytes =
+                MessageDigest.getInstance("SHA-256").digest(contrasenaEncriptada.toByteArray())
+            return bytes.joinToString("") { "%02x".format(it) }
         }
 
         btnCrearRegistro.setOnClickListener {
@@ -76,78 +78,117 @@ class activity_registrarse : AppCompatActivity() {
             val contrasena = txtContrasena1.text.toString()
             val duiRegex = Regex("^\\d{8}-\\d\$")
             val telefonoRegex = Regex("^\\d{4}-\\d{4}\$")
-            val correoPattern = Regex ("[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
+            val correoPattern = Regex("[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
             val contrasenaRegex = Regex("^(?=.*[0-9!@#\$%^&*()-_=+\\|\\[{\\]};:'\",<.>/?]).{6,}\$")
 
-            if (dui.isEmpty() || tipoDeSangre.isEmpty() || tel.isEmpty() || correo.isEmpty() || contrasena.isEmpty()) {
+            var valid = true
 
-                Toast.makeText(
-                    this@activity_registrarse,
-                    "Error, para crear la cuenta debe llenar todas las casillas.",
-                    Toast.LENGTH_SHORT
-                ).show()
+            if (dui.isEmpty()) {
+                txtDUI.setError("El DUI no puede estar vacío")
+                valid = false
             } else if (!duiRegex.matches(dui)) {
-                Toast.makeText(
-                    this@activity_registrarse,
-                    "Error, el DUI no es válido. Debe tener el formato adecuado, por ejemplo, 12345678-9.",
-                    Toast.LENGTH_SHORT
-                ).show()
+                txtDUI.setError("Error, el DUI no es valido. Debe tener el formato adecuado, por ejemplo, 12345678-9.")
+                valid = false
+            }
+
+            if (tel.isEmpty()) {
+                txtTelefono.setError("El teléfono no puede estar vacío")
+                valid = false
             } else if (!telefonoRegex.matches(tel)) {
-                Toast.makeText(
-                    this@activity_registrarse,
-                    "Error, El número de teléfono no es válido.",
-                    Toast.LENGTH_SHORT
-                ).show()
+                txtTelefono.setError("Error, el número de teléfono no es válido. Debe tener el formato adecuado, por ejemplo, 1234-5678.")
+                valid = false
+            }
+
+            if (correo.isEmpty()) {
+                txtCorreo1.setError("El correo no puede estar vacío")
+                valid = false
             } else if (!correoPattern.matches(correo)) {
-                Toast.makeText(
-                    this@activity_registrarse,
-                    "Error, el correo electrónico no es válido.",
-                    Toast.LENGTH_SHORT
-                ).show()
+                txtCorreo1.setError("Error, el correo eléctronico no es válido. Debe contener el formato adecuado, por ejemplo, ejemplo@gmail.com.")
+                valid = false
+            }
+
+            if (contrasena.isEmpty()) {
+                txtContrasena1.setError("La contraseña no puede estar vacía")
+                valid = false
             } else if (!contrasenaRegex.matches(contrasena)) {
-                Toast.makeText(
-                    this@activity_registrarse,
-                    "Error, la contraseña debe contener al menos un caracter especial y tener más de 6 caracteres.",
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else {
+                txtContrasena1.setError("Error la contraseña debe contener al menos un caracter especial y tener más de 6 caracteres.")
+                valid = false
+            }
+
+            if (valid) {
                 CoroutineScope(Dispatchers.IO).launch {
-
                     try {
-
                         val objConexion = ClaseConexion().cadenaConexion()
 
-                        val contrasenaEncriptacion = hashSHA256(txtContrasena1.text.toString())
+                        val checkDuplicados =
+                            objConexion?.prepareStatement("SELECT * FROM Usuarios WHERE dui = ? OR telefono = ?")
+                        checkDuplicados?.setString(1, dui)
+                        checkDuplicados?.setString(2, tel)
+                        val resultSet = checkDuplicados?.executeQuery()
 
-                        val addUsuarios =
-                            objConexion?.prepareStatement("insert into Usuarios (dui, tipo_sangre, telefono, correo_electronico, contrasena) values (?, ?, ?, ?, ?)")!!
-                        addUsuarios.setString(1, txtDUI.text.toString())
-                        addUsuarios.setString(2, spTipoSangre.selectedItem.toString())
-                        addUsuarios.setString(3, txtTelefono.text.toString())
-                        addUsuarios.setString(4, txtCorreo1.text.toString())
-                        addUsuarios.setString(5, contrasenaEncriptacion)
+                        if (resultSet != null && resultSet.next()) {
+                            withContext(Dispatchers.Main) {
+                                val duiExistente = resultSet.getString("dui")
+                                val telefonoExistente = resultSet.getString("telefono")
+                                if (duiExistente == dui) {
+                                    AlertDialog.Builder(this@activity_registrarse)
+                                        .setTitle("Error de registro")
+                                        .setMessage("El DUI ya existe. Por favor, ingrese otro.")
+                                        .setPositiveButton("Aceptar", null)
+                                        .show()
+                                }
+                                if (telefonoExistente == tel) {
+                                    AlertDialog.Builder(this@activity_registrarse)
+                                        .setTitle("Error de registro")
+                                        .setMessage("El teléfono ya ha sido empleado. Por favor, elige otro.")
+                                        .setPositiveButton("Aceptar", null)
+                                        .show()
+                                }
+                            }
+                        } else {
+                            CoroutineScope(Dispatchers.IO).launch {
 
-                        addUsuarios.executeUpdate()
+                                try {
 
-                        withContext(Dispatchers.Main) {
-                            AlertDialog.Builder(this@activity_registrarse)
-                                .setTitle("Registro exitoso")
-                                .setMessage("La cuenta se ha creado exitosamente.")
-                                .setPositiveButton("Aceptar", null)
-                                .show()
-                            txtDUI.setText("")
-                            txtTelefono.setText("")
-                            txtCorreo1.setText("")
-                            txtContrasena1.setText("")
+                                    val objConexion = ClaseConexion().cadenaConexion()
+
+                                    val contrasenaEncriptacion =
+                                        hashSHA256(txtContrasena1.text.toString())
+
+                                    val addUsuarios =
+                                        objConexion?.prepareStatement("insert into Usuarios (dui, tipo_sangre, telefono, correo_electronico, contrasena) values (?, ?, ?, ?, ?)")!!
+                                    addUsuarios.setString(1, txtDUI.text.toString())
+                                    addUsuarios.setString(2, spTipoSangre.selectedItem.toString())
+                                    addUsuarios.setString(3, txtTelefono.text.toString())
+                                    addUsuarios.setString(4, txtCorreo1.text.toString())
+                                    addUsuarios.setString(5, contrasenaEncriptacion)
+
+                                    addUsuarios.executeUpdate()
+
+                                    withContext(Dispatchers.Main) {
+                                        AlertDialog.Builder(this@activity_registrarse)
+                                            .setTitle("Registro exitoso")
+                                            .setMessage("La cuenta se ha creado exitosamente.")
+                                            .setPositiveButton("Aceptar", null)
+                                            .show()
+                                        txtDUI.setText("")
+                                        txtTelefono.setText("")
+                                        txtCorreo1.setText("")
+                                        txtContrasena1.setText("")
+                                    }
+                                } catch (e: java.sql.SQLIntegrityConstraintViolationException) {
+                                    withContext(Dispatchers.Main) {
+                                        AlertDialog.Builder(this@activity_registrarse)
+                                            .setTitle("Error de registro")
+                                            .setMessage("El correo electronico ya ha sido empleada. Por favor, elige otro.")
+                                            .setPositiveButton("Aceptar", null)
+                                            .show()
+                                    }
+                                }
+                            }
                         }
-                    }catch (e: java.sql.SQLIntegrityConstraintViolationException){
-                        withContext(Dispatchers.Main) {
-                            AlertDialog.Builder(this@activity_registrarse)
-                                .setTitle("Error de registro")
-                                .setMessage("El correo electronico ya ha sido empleada. Por favor, elige otro.")
-                                .setPositiveButton("Aceptar", null)
-                                .show()
-                        }
+                    }catch (e: Exception){
+                        println(e)
                     }
                 }
             }
