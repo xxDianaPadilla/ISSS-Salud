09d5db69939cb54625ed6519c18942da1f157fd6
xxDianaPadilla <dianagabypadilla006@gmail.com>
2024-10-12 08:39:15 -0600

    Pase 3 horas configurando esto del PDF y las notificaciones

---
+++ app/src/main/java/Modelo/ExpedienteMedico.kt
@@ -0,0 +1,19 @@
+package Modelo
+
+import java.sql.Date
+
+data class ExpedienteMedico(
+    val nombreUsuario: String,
+    val dui: String,
+    val sexo: String,
+    val edad: Date,
+    val tipoSangre: String,
+    val telefono: String,
+    val correoElectronico: String,
+    val antecedentesFamiliares: String,
+    val problemasSaludPreexistentes: String,
+    val alergias: String,
+    val saludActual: String,
+    val resultadosExamenesLaboratorio: String,
+    val fichaIngreso: String
+)
--- app/src/main/java/diana/padilla/isss_salud/activity_perfil.kt
+++ app/src/main/java/diana/padilla/isss_salud/activity_perfil.kt
@@ -1,17 +1,32 @@
 package diana.padilla.isss_salud
 
 import Modelo.ClaseConexion
+import Modelo.ExpedienteMedico
 import Modelo.Perfil
+import android.app.AlertDialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.graphics.Paint
+import android.graphics.pdf.PdfDocument
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bumptech.glide.Glide
@@ -19,6 +34,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
 
 class activity_perfil : AppCompatActivity() {
 
@@ -27,7 +44,7 @@ class activity_perfil : AppCompatActivity() {
     lateinit var telefonoPerfilE: EditText
     lateinit var duiPerfilE: EditText
     lateinit var tipoSangreE: EditText
-    lateinit var openLinkButton: Button
+    lateinit var openExpediente: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,18 +57,13 @@ class activity_perfil : AppCompatActivity() {
             insets
         }
 
-        openLinkButton = findViewById(R.id.btnDescargarExpediente)
+        openExpediente = findViewById(R.id.btnDescargarExpediente)
 
-        openLinkButton.setOnClickListener {
-            val url = "https://www.canva.com/design/DAGOuctkzZg/uAXshmbnec-xCLQHHWmJCw/view?utm_content=DAGOuctkzZg&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse(url)
-            startActivity(intent)
+        openExpediente.setOnClickListener {
+            generarExpedientePDF()
         }
 
-
         cargarDatosPerfilEnPantalla()
-
 
         imagenPerfil = findViewById<ImageView>(R.id.ImgPerfilSinCargar)
         correoPerfilE = findViewById<EditText>(R.id.txtViewCorreoPerfilE)
@@ -166,6 +178,42 @@ class activity_perfil : AppCompatActivity() {
         return perfil
     }
 
+    suspend fun obtenerExpedienteMedico(idUsuario: Int): ExpedienteMedico? {
+        var expediente: ExpedienteMedico? = null
+        withContext(Dispatchers.IO) {
+            val objConexion = ClaseConexion().cadenaConexion()
+            val obtenerExpediente = objConexion?.prepareStatement("""
+            SELECT u.nombre_usuario, u.dui, u.sexo, u.edad, u.tipo_sangre, u.telefono, u.correo_electronico, 
+                   e.antecedentes_familiares, e.problemas_salud_preexistentes, e.alergias, 
+                   e.salud_actual, e.resultados_examenes_laboratorio, e.ficha_ingreso
+            FROM Usuarios u
+            INNER JOIN ExpedientesMedicos e ON u.id_usuario = e.id_usuario
+            WHERE u.id_usuario = ?
+        """)
+            obtenerExpediente?.setInt(1, idUsuario)
+            val resultSet = obtenerExpediente?.executeQuery()
+
+            if (resultSet?.next() == true) {
+                expediente = ExpedienteMedico(
+                    resultSet.getString("nombre_usuario"),
+                    resultSet.getString("dui"),
+                    resultSet.getString("sexo"),
+                    resultSet.getDate("edad"),
+                    resultSet.getString("tipo_sangre"),
+                    resultSet.getString("telefono"),
+                    resultSet.getString("correo_electronico"),
+                    resultSet.getString("antecedentes_familiares"),
+                    resultSet.getString("problemas_salud_preexistentes"),
+                    resultSet.getString("alergias"),
+                    resultSet.getString("salud_actual"),
+                    resultSet.getString("resultados_examenes_laboratorio"),
+                    resultSet.getString("ficha_ingreso")
+                )
+            }
+        }
+        return expediente
+    }
+
     fun cargarDatosPerfilEnPantalla() {
         CoroutineScope(Dispatchers.Main).launch {
             val correoDeLaVariableGlobal = activity_ingreso.variablesGlobales.miMorreo
@@ -184,6 +232,104 @@ class activity_perfil : AppCompatActivity() {
                 telefonoPerfilE.hint = miTelefono
                 val miTipoSangre = perfiles[0].tipo_sangre
                 tipoSangreE.hint = miTipoSangre
+            }
+        }
+    }
+
+    private fun generarExpedientePDF() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val idUsuario = activity_ingreso.variablesGlobales.idUsuarioGlobal // Obtener el id_usuario de la variable global
+            val expediente = obtenerExpedienteMedico(idUsuario)
+
+            if (expediente != null) {
+                val pdfDocument = PdfDocument()
+                val pageInfo = PdfDocument.PageInfo.Builder(595, 842, 1).create()
+                val page = pdfDocument.startPage(pageInfo)
+
+                val canvas = page.canvas
+                val paint = Paint()
+                paint.textSize = 16f
+
+                // Agrega datos al PDF
+                canvas.drawText("Nombre: ${expediente.nombreUsuario}", 50f, 50f, paint)
+                canvas.drawText("DUI: ${expediente.dui}", 50f, 80f, paint)
+                canvas.drawText("Sexo: ${expediente.sexo}", 50f, 110f, paint)
+                canvas.drawText("Edad: ${expediente.edad}", 50f, 140f, paint)
+                canvas.drawText("Tipo de sangre: ${expediente.tipoSangre}", 50f, 170f, paint)
+                canvas.drawText("Teléfono: ${expediente.telefono}", 50f, 200f, paint)
+                canvas.drawText("Correo: ${expediente.correoElectronico}", 50f, 230f, paint)
+                canvas.drawText("Antecedentes familiares: ${expediente.antecedentesFamiliares}", 50f, 260f, paint)
+                canvas.drawText("Problemas de salud preexistentes: ${expediente.problemasSaludPreexistentes}", 50f, 290f, paint)
+                canvas.drawText("Alergias: ${expediente.alergias}", 50f, 320f, paint)
+                canvas.drawText("Salud actual: ${expediente.saludActual}", 50f, 350f, paint)
+                canvas.drawText("Resultados de exámenes: ${expediente.resultadosExamenesLaboratorio}", 50f, 380f, paint)
+                canvas.drawText("Ficha de ingreso: ${expediente.fichaIngreso}", 50f, 410f, paint)
+
+                pdfDocument.finishPage(page)
+
+                // Guardar PDF en el almacenamiento del dispositivo
+                val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "Expediente_Medico_$idUsuario.pdf")
+                pdfDocument.writeTo(FileOutputStream(file))
+                pdfDocument.close()
+
+                mostrarNotificacion(file)
+
+                // Mostrar un AlertDialog de éxito
+                AlertDialog.Builder(this@activity_perfil)
+                    .setTitle("Descarga exitosa")
+                    .setMessage("El expediente médico ha sido descargado correctamente.")
+                    .setPositiveButton("OK", null)
+                    .show()
+            } else {
+                Toast.makeText(this@activity_perfil, "No se pudo obtener el expediente médico.", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun mostrarNotificacion(file: File) {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = "mi_canal"
+
+        // Crear el canal de notificación (si no se ha creado anteriormente)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, "Nombre del canal", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        // Crea un intent para abrir el archivo PDF
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            setDataAndType(FileProvider.getUriForFile(this@activity_perfil, "diana.padilla.isss_salud.fileprovider", file), "application/pdf")
+            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION // Permitir el acceso de lectura al archivo
+        }
+
+        // Crear el PendingIntent con FLAG_IMMUTABLE
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+
+        // Construir la notificación
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_darkmode_notificaciones) // Cambia esto por tu ícono de notificación
+            .setContentTitle("Descarga completada")
+            .setContentText("Tu expediente médico ha sido descargado.")
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true) // La notificación se eliminará al tocarla
+            .build()
+
+        // Mostrar la notificación
+        notificationManager.notify(1, notification)
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 1) {
+            // Verificar si el permiso fue concedido
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // Permiso concedido, generar el PDF
+                generarExpedientePDF()
+            } else {
+                // Permiso denegado, mostrar mensaje
+                Toast.makeText(this, "Permiso de escritura denegado", Toast.LENGTH_LONG).show()
             }
         }
     }
